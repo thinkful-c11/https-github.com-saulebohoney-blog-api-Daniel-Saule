750320ff9d887609eb083a4d265b6e26763b8fa5
Saule BN <beyonce@thinkful.com>
2017-05-24 17:48:35 -0400

    added PUT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const uuid = require('uuid');
 const morgan = require('morgan');
 const router = express.Router();
 const app = express();
-const jsonParser = require("body-parser").json();
+const jsonParser = require('body-parser').json();
 
 const {BlogPosts} = require('./models.js');
 
@@ -23,8 +23,35 @@ app.post('/blog-posts', (req, res) => {
     res.json(BlogPosts.create(title, content, author));
 });
 
-app.delete('/blog-posts',(req,res)=>{res.json(BlogPosts.delete());});
+app.put('/blog-posts/:id',jsonParser, (req,res)=>{
+  res.json(BlogPosts.update());
+
+//     const requiredFields = ['title', 'content', 'author'];
+//     for (let i=0; i<requiredFields.length; i++) {
+//         const field = requiredFields[i];
+//         if (!(field in req.body)) {
+//             const message = `Missing \`${field}\` in request body`;
+//     //console.error(message);
+//             return res.status(400).send(message);
+//         }
+//     }
+//     if (req.params.id !== req.body.id) {
+//         const message = (`Request path id (${req.params.id}) and request body id `
+//      `(${req.body.id}) must match`);
+// //  console.error(message);
+//         return res.status(400).send(message);
+//     }
+//     // console.log(`Updating shopping list item \`${req.params.id}\``);
+//     BlogPosts.update({
+//         id: req.params.id,
+//         title: req.body.title,
+//         content: req.body.content,
+//         author: req.body.author
+//     });
+//     res.status(204).end();
+});
+app.delete('/blog-posts/:id',(req,res)=>{res.json(BlogPosts.delete());});
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
