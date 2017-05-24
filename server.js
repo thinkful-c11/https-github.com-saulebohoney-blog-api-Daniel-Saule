c2b83307c67d7b557c97fa27f7bd77bcb668ddbc
dankato <apearmor@gmail.com>
2017-05-24 13:13:27 -0700

    updates to server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const uuid = require('uuid');
+const morgan = require('morgan');
+const router = express.Router();
+const app = express();
+// const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models.js');
+
+
+app.use(morgan('common'));
+// BlogPost.create();
+
+
+BlogPosts.create("horse", "test", "saule");
+
+app.get('/blog-post', (req, res) => {
+
+    res.json(BlogPosts.get());
+    
+});
+
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
