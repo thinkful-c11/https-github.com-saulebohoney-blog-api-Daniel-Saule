968b3b0787ccaa7db2226db5adad2b693f30e997
dankato <apearmor@gmail.com>
2017-05-24 14:20:30 -0700

    we now can post

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ const uuid = require('uuid');
 const morgan = require('morgan');
 const router = express.Router();
 const app = express();
-// const jsonParser = bodyParser.json();
+const jsonParser = require("body-parser").json();
 
 const {BlogPosts} = require('./models.js');
 
-
 app.use(morgan('common'));
+app.use(jsonParser);
 
 BlogPosts.create('horse','test','saule');
 BlogPosts.create('milk','test','Sandy');
 
-app.get('/blog-post', (req, res) => {
-
+app.get('/blog-posts', (req, res) => {
     res.json(BlogPosts.get());
-    
 });
 
+app.post('/blog-posts', (req, res) => {
+    // console.log(req.body);
+    const {title,content,author} = req.body;
+    res.json(BlogPosts.create(title, content, author));
+});
 
-app.delete('/blog-post',(req,res)=>{res.json(BlogPosts.delete());});
+app.delete('/blog-posts',(req,res)=>{res.json(BlogPosts.delete());});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
