8f59c9b0dd0f9f9ddfabcd0aed9f02098231ae0f
Saule BN <beyonce@thinkful.com>
2017-05-24 23:19:10 -0400

    one error message isn't working

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const {BlogPosts} = require('./models.js');
 app.use(morgan('common'));
 app.use(jsonParser);
 
-BlogPosts.create('horse','test','saule');
-BlogPosts.create('milk','test','Sandy');
 
 app.get('/blog-posts', (req, res) => {
     res.json(BlogPosts.get());
@@ -24,32 +22,34 @@ app.post('/blog-posts', (req, res) => {
 });
 
 app.put('/blog-posts/:id',jsonParser, (req,res)=>{
-  res.json(BlogPosts.update());
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = (`Request path id (${req.params.id}) and request body id ``(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
+    }
+        console.log(`Updating blog-post item \`${req.params.id}\``);
 
-//     const requiredFields = ['title', 'content', 'author'];
-//     for (let i=0; i<requiredFields.length; i++) {
-//         const field = requiredFields[i];
-//         if (!(field in req.body)) {
-//             const message = `Missing \`${field}\` in request body`;
-//     //console.error(message);
-//             return res.status(400).send(message);
-//         }
-//     }
-//     if (req.params.id !== req.body.id) {
-//         const message = (`Request path id (${req.params.id}) and request body id `
-//      `(${req.body.id}) must match`);
-// //  console.error(message);
-//         return res.status(400).send(message);
-//     }
-//     // console.log(`Updating shopping list item \`${req.params.id}\``);
-//     BlogPosts.update({
-//         id: req.params.id,
-//         title: req.body.title,
-//         content: req.body.content,
-//         author: req.body.author
-//     });
-//     res.status(204).end();
+    const updatedItem = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+        publishDate: req.body.publishDate
+    });
+
+    res.json(updatedItem);
+
 });
+
 app.delete('/blog-posts/:id',(req,res)=>{res.json(BlogPosts.delete());});
 
 app.listen(process.env.PORT || 8080, () => {
